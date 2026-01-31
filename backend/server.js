0f3ec420a283344dd10b6b7e1d64997a5f941742
kushdeveloper68 <kushpandit68775@gmail.com>
2026-01-31 18:39:09 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,24 @@ const mongooseConnection = require("./connections/connection");
 const router = require("./routes/routes");
  mongooseConnection(process.env.MONGOURL || "mongodb://localhost:27017/localstore");
 
+ const allowedOrigins = [
+   "http://localhost:5173",
+   "https://lkmart.onrender.com"
+ ];
+ 
+ app.use(cors({
+   origin: function (origin, callback) {
+     // allow requests with no origin (like mobile apps or curl)
+     if (!origin) return callback(null, true);
+ 
+     if (allowedOrigins.includes(origin)) {
+       callback(null, true);
+     } else {
+       callback(new Error("Not allowed by CORS"));
+     }
+   },
+   credentials: true
+ }));
  app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
  app.use(bodyParser.json())
@@ -20,10 +38,6 @@ app.use(express.urlencoded({ extended: true }));
 // Debug middleware
 
 
-app.use(cors({
-  origin: "https://lkmart.onrender.com" || "http://localhost:5173", //  frontend URL
-  credentials: true
-}));
 
 app.use("/api", router);
 
