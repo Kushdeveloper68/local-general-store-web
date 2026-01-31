69e028d692a022843f40c02466df9d7d1a1d6b6d
kushdeveloper68 <kushpandit68775@gmail.com>
2026-01-31 18:26:47 +0530

    url add to backend

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: "http://localhost:5173", //  frontend URL
+  origin: "https://lkmart.onrender.com/", //  frontend URL
   credentials: true
 }));
 
