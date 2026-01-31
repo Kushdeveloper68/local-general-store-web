b01143d36dc0f10ab90848cce3a0aa510a217934
kushdeveloper68 <kushpandit68775@gmail.com>
2026-01-31 18:34:56 +0530

    traling /

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'https://local-general-store-web.onrender.com/',
+  baseURL: 'https://local-general-store-web.onrender.com',
   withCredentials: true,
 });
 
